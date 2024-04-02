25af8d3d069d65c47cd68aca65edcd8140dfc836
Volodymyr Hyrka <vov4uk.xorowuj@gmail.com>
2024-04-02 20:58:54 +0300

    Add generic T

--- lib/src/logic/shorts_controller.dart
+++ lib/src/logic/shorts_controller.dart
@@ -11,7 +11,7 @@ import 'package:youtube_shorts/src/source/interface_videos_source_controller.dar
 
 part 'mixin_video_control_shortcut.dart';
 
-class _MyValueNotifier extends ValueNotifier<dynamic> {
+class _MyValueNotifier extends ValueNotifier<T> {
   bool _disposed = false;
 
   @override
