8149755af8fefaf3ba51704648146bd9c59753bb
Volodymyr Hyrka <vov4uk.xorowuj@gmail.com>
2024-04-02 21:08:40 +0300

    Fix generic

--- lib/src/logic/shorts_controller.dart
+++ lib/src/logic/shorts_controller.dart
@@ -11,7 +11,7 @@ import 'package:youtube_shorts/src/source/interface_videos_source_controller.dar
 
 part 'mixin_video_control_shortcut.dart';
 
-class _MyValueNotifier<T> extends ValueNotifier {
+class _MyValueNotifier<T> extends ValueNotifier<T> {
   bool _disposed = false;
 
   @override
